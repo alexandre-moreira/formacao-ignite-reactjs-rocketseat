8bb47916abec8fadcbfa66ae954c2c9a5282199d
Alexandre Moreira <4lexandre.moreira@gmail.com>
2022-04-25 16:34:04 -0300

    Criando o componente RepositoryList

---
+++ Chapter I/01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,37 @@
+export function RepositoryList(){
+    return(
+        <section className="repository-list">
+            <h1>Lista de repositor</h1>
+            <ul>
+                <li>
+                    <strong>uform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+
+                <li>
+                    <strong>uform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+
+                <li>
+                    <strong>uform</strong>
+                    <p>Forms in React</p>
+
+                    <a href="">
+                        Acessar repositório
+                    </a>
+                </li>
+            </ul>
+
+
+        </section>
+    )
+}
